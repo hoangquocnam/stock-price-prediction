383de7ce5e4a60304b993fff17ce87e0c4e0f249
hqnam <hoangquocnam021117@gmail.com>
2024-09-04 18:55:08 +0700

    fix: fix stock app to run 3 models + implement dropdown

--- stock_app.py
+++ stock_app.py
@@ -1,6 +1,6 @@
 import dash
-from dash import dcc
 from dash import html
+from dash import dcc
 import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
@@ -8,140 +8,116 @@ from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
-
 app = dash.Dash()
 server = app.server
 
-scaler=MinMaxScaler(feature_range=(0,1))
+cryptos = ['ADA', 'BTC', 'ETH']
 
-# Load datasets for different cryptocurrencies
-btc_df = pd.read_csv("./datasets/BTC-USD.csv")
-eth_df = pd.read_csv("./datasets/ETH-USD.csv")
-ada_df = pd.read_csv("./datasets/ADA-USD.csv")
+# Function to load and prepare data
+def load_data(crypto):
+    data = pd.read_csv(f'./data/processed/{crypto}_processed.csv')
+    data['Date'] = pd.to_datetime(data['Date'])
+    data.set_index('Date', inplace=True)
+    return data
 
-# Function to process and prepare data
-def prepare_data(df):
-    df["Date"] = pd.to_datetime(df.Date, format="%Y-%m-%d")
-    df.index = df['Date']
-    data = df.sort_index(ascending=True, axis=0)
-    new_data = pd.DataFrame(index=range(0, len(df)), columns=['Date', 'Close'])
-    for i in range(0, len(data)):
-        new_data["Date"][i] = data['Date'][i]
-        new_data["Close"][i] = data["Close"][i]
-    new_data.index = new_data.Date
-    new_data.drop("Date", axis=1, inplace=True)
-    return new_data
+# Function to predict prices
+def predict_prices(crypto):
+    data = load_data(crypto)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    final_data = data[['Close']].values
+    scaled_data = scaler.fit_transform(final_data)
 
-# Prepare data for each cryptocurrency
-btc_data = prepare_data(btc_df)
-eth_data = prepare_data(eth_df)
-ada_data = prepare_data(ada_df)
+    train_size = int(len(scaled_data) * 0.7)
+    valid_data = scaled_data[train_size:]
 
-# Load your pre-trained model
-model = load_model("saved_model.h5")
+    x_valid, y_valid = [], []
+    for i in range(60, len(valid_data)):
+        x_valid.append(valid_data[i-60:i, 0])
+        y_valid.append(final_data[train_size + i, 0])
 
-def get_predictions(data, model, scaler):
-    dataset = data.values
-    train = dataset[0:987, :]
-    valid = dataset[987:, :]
+    x_valid = np.array(x_valid)
+    y_valid = np.array(y_valid)
+    x_valid = np.reshape(x_valid, (x_valid.shape[0], x_valid.shape[1], 1))
 
-    scaled_data = scaler.fit_transform(dataset)
-    x_train, y_train = [], []
+    model = load_model(f'./models/saved_models/{crypto}_model.h5')
+    predictions = model.predict(x_valid)
+    predictions = scaler.inverse_transform(predictions).flatten()
+    return data.index[train_size+60:], y_valid, predictions
 
-    for i in range(60, len(train)):
-        x_train.append(scaled_data[i-60:i, 0])
-        y_train.append(scaled_data[i, 0])
-
-    x_train, y_train = np.array(x_train), np.array(y_train)
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-    inputs = data[len(data) - len(valid) - 60:].values
-    inputs = inputs.reshape(-1, 1)
-    inputs = scaler.transform(inputs)
-
-    X_test = []
-    for i in range(60, inputs.shape[0]):
-        X_test.append(inputs[i-60:i, 0])
-    X_test = np.array(X_test)
-
-    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    closing_price = model.predict(X_test)
-    closing_price = scaler.inverse_transform(closing_price)
-    
-    # Check the shapes of valid and closing_price
-    print(f"Shape of valid: {valid.shape}")
-    print(f"Shape of closing_price: {closing_price.shape}")
-    
-    # Make sure valid and closing_price have matching shapes
-    valid = pd.DataFrame(valid, columns=['Close'])  # Ensure valid is a DataFrame
-    valid['Predictions'] = closing_price[:len(valid)]  # Truncate to match the valid length
-
-    return valid
-
-# Dash Layout
 app.layout = html.Div([
-    html.H1("Cryptocurrency Price Prediction Dashboard", style={"textAlign": "center"}),
+    html.H1("Stock Price Dashboard", style={"textAlign": "center"}),
 
-    # Dropdown for selecting currency pair
-    dcc.Dropdown(
-        id='crypto-dropdown',
-        options=[
-            {'label': 'BTC-USD', 'value': 'BTC'},
-            {'label': 'ETH-USD', 'value': 'ETH'},
-            {'label': 'ADA-USD', 'value': 'ADA'}
-        ],
-        value='BTC',  # default value
-        style={"width": "50%", "margin": "auto"}
-    ),
+    dcc.Tabs(id="tabs", children=[
 
-    html.Div(id='crypto-graphs')
+        dcc.Tab(label='Stock Price Prediction', children=[
+            html.Div([
+                # html.H2("ADA Price Prediction", style={"textAlign": "center"}),
+                dcc.Graph(id='ada-prediction'),
+                # html.H2("BTC Price Prediction", style={"textAlign": "center"}),
+                dcc.Graph(id='btc-prediction'),
+                # html.H2("ETH Price Prediction", style={"textAlign": "center"}),
+                dcc.Graph(id='eth-prediction')
+            ])
+        ]),
+
+        dcc.Tab(label='Stock Price Analytics', children=[
+            html.Div([
+                html.H1("Stock Price Analytics", style={'textAlign': 'center'}),
+                dcc.Dropdown(id='crypto-dropdown',
+                             options=[{'label': crypto, 'value': crypto} for crypto in cryptos],
+                             value='BTC', style={"width": "50%", "margin": "auto"}),
+                dcc.Graph(id='high-low-chart'),
+                dcc.Graph(id='open-close-chart')
+            ])
+        ])
+    ])
 ])
 
-
-# Callback to update graphs based on selected cryptocurrency
 @app.callback(
-    Output('crypto-graphs', 'children'),
+    [Output('ada-prediction', 'figure'),
+     Output('btc-prediction', 'figure'),
+     Output('eth-prediction', 'figure')],
+    [Input('tabs', 'value')]
+)
+def update_predictions(_):
+    figures = []
+    for crypto in cryptos:
+        dates, actual, predicted = predict_prices(crypto)
+        fig = {
+            "data": [
+                go.Scatter(x=dates, y=actual, mode='lines', name='Actual', line=dict(color='orange')),
+                go.Scatter(x=dates, y=predicted, mode='lines', name='Predicted', line=dict(color='blue'))
+            ],
+            "layout": go.Layout(title=f'{crypto} Price Prediction', xaxis={'title': 'Date'}, yaxis={'title': 'Price'})
+        }
+        figures.append(fig)
+    return figures
+
+@app.callback(
+    [Output('high-low-chart', 'figure'),
+     Output('open-close-chart', 'figure')],
     [Input('crypto-dropdown', 'value')]
 )
-def update_crypto_graphs(selected_crypto):
-    if selected_crypto == 'BTC':
-        data = btc_data
-    elif selected_crypto == 'ETH':
-        data = eth_data
-    else:
-        data = ada_data
+def update_analytics(selected_crypto):
+    data = load_data(selected_crypto)
 
-    valid = get_predictions(data, model, scaler)
+    high_low_fig = {
+        "data": [
+            go.Scatter(x=data.index, y=data['High'], mode='lines', name='High', line=dict(color='green')),
+            go.Scatter(x=data.index, y=data['Low'], mode='lines', name='Low', line=dict(color='red'))
+        ],
+        "layout": go.Layout(title=f'{selected_crypto} High vs Low Prices', xaxis={'title': 'Date'}, yaxis={'title': 'Price'})
+    }
 
-    return html.Div([
-        html.H2(f"{selected_crypto}-USD Actual vs Predicted Closing Price", style={"textAlign": "center"}),
-        
-        dcc.Graph(
-            id=f'{selected_crypto}-actual',
-            figure={
-                "data": [
-                    go.Scatter(
-                        x=data.index,
-                        y=data["Close"],
-                        mode='lines',
-                        name='Actual'
-                    ),
-                    go.Scatter(
-                        x=valid.index,
-                        y=valid["Predictions"],
-                        mode='lines',
-                        name='Predicted'
-                    )
-                ],
-                "layout": go.Layout(
-                    title=f"{selected_crypto}-USD Closing Price Prediction",
-                    xaxis={'title': 'Date'},
-                    yaxis={'title': 'Closing Price (USD)'}
-                )
-            }
-        )
-    ])
+    open_close_fig = {
+        "data": [
+            go.Scatter(x=data.index, y=data['Open'], mode='lines', name='Open', line=dict(color='purple')),
+            go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Close', line=dict(color='black'))
+        ],
+        "layout": go.Layout(title=f'{selected_crypto} Open vs Close Prices', xaxis={'title': 'Date'}, yaxis={'title': 'Price'})
+    }
 
+    return high_low_fig, open_close_fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
